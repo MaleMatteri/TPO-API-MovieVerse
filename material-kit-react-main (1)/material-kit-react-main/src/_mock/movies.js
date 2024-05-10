23e8f58f04220fc229c27d4987d5807c4830ce0d
Male Matteri <mjmatteri@gmail.com>
2024-05-09 21:18:08 -0300

    Update movies.js
    Se agregaron las peliculas a movie.js

--- material-kit-react-main (1)/material-kit-react-main/src/_mock/movies.js
+++ material-kit-react-main (1)/material-kit-react-main/src/_mock/movies.js
@@ -1,29 +1,5 @@
 import { faker } from '@faker-js/faker';
-
-// ----------------------------------------------------------------------
-
-// const MOVIE_NAME = [
-//     'Mamma mia',
-//     'Mamma mia: Here we go again',
-//     'Tokyo Drift',
-//     'One day',
-//     'Star Wars: The Force Awakens',
-//     'Aladdin',
-//     'Friends With Benefits',
-//     'Safe Heaven',
-//     'Star Wars: The last Jedi',
-//     'The Visitation',
-//     'Harry Potter and the half-blood prince',
-//     'Anyone but you',
-//     'Elements',
-//     'Interstellar',
-//     'The Matrix',
-//     'The Princess Diaries',
-//     'Maleficent: Mistress of evil',
-//     'Beauty and the Beast',
-//     'Wish',
-//     'Matilda',
-// ];
+//------------------------------------------------------------
 
 const MOVIE_DATA = [
   {
@@ -42,7 +18,72 @@ const MOVIE_DATA = [
     name: 'One Day',
     mainActors: ['Leo Woodall, ', 'Ambika Mod, ', 'Amber Grappy'],
   },
+  {
+    name: 'Star Wars: The Force Awakens',
+    mainActors: ['Daisy Ridley, ', 'John Boyega, ', 'Adam Driver'],
+  },
+  {
+    name: 'Aladdin',
+    mainActors: ['Will Smith, ', 'Mena Massoud, ', 'Naomi Scott'],
+  },
+  {
+    name: 'Friends With Benefits',
+    mainActors: ['Justin Timberlake, ', 'Mila Kunis, ', 'Patricia Clarkson'],
+  },
+  {
+    name: 'Safe Haven',
+    mainActors: ['Julianne Hough, ', 'Josh Duhamel, ', 'Cobie Smulders'],
+  },
+  {
+    name: 'Star Wars: The Last Jedi',
+    mainActors: ['Mark Hamill, ', 'Carrie Fisher, ', 'Adam Driver'],
+  },
+  {
+    name: 'The Visitation',
+    mainActors: ['Edward Furlong, ', 'Kelly Lynch, ', 'Randy Travis'],
+  },
+  {
+    name: 'Harry Potter and the Half-Blood Prince',
+    mainActors: ['Daniel Radcliffe, ', 'Emma Watson, ', 'Rupert Grint'],
+  },
+  {
+    name: 'Anyone But You',
+    mainActors: ['Joshua Garcia, ', 'Barbie Imperial, ', 'Mccoy De Leon'],
+  },
+  {
+    name: 'Elements',
+    mainActors: ['Charlie David, ', 'Brad Alphonso, ', 'Vincent Bilancio'],
+  },
+  {
+    name: 'Interstellar',
+    mainActors: ['Matthew McConaughey, ', 'Anne Hathaway, ', 'Jessica Chastain'],
+  },
+  {
+    name: 'The Matrix',
+    mainActors: ['Keanu Reeves, ', 'Laurence Fishburne, ', 'Carrie-Anne Moss'],
+  },
+  {
+    name: 'The Princess Diaries',
+    mainActors: ['Anne Hathaway, ', 'Julie Andrews, ', 'Heather Matarazzo'],
+  },
+  {
+    name: 'Maleficent: Mistress of Evil',
+    mainActors: ['Angelina Jolie, ', 'Elle Fanning, ', 'Michelle Pfeiffer'],
+  },
+  {
+    name: 'Beauty and the Beast',
+    mainActors: ['Emma Watson, ', 'Dan Stevens, ', 'Luke Evans'],
+  },
+  {
+    name: 'Wish',
+    mainActors: ['Joel Courtney, ', 'Danielle Rose Russell, ', 'Skyler Gisondo'],
+  },
+  {
+    name: 'Matilda',
+    mainActors: ['Mara Wilson, ', 'Danny DeVito, ', 'Rhea Perlman'],
+  },
 ];
+
 
 const movies = MOVIE_DATA.map((movieData, index) => ({
   id: faker.datatype.uuid(),
